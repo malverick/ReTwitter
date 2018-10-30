93267011908fbfd63202df93bc3ba65a9550d6af
Sumit Madhwani <sumitlnmiit96@gmail.com>
2018-10-31 02:06:56 +0530

    make changes to deploy

--- ReTwitter/settings.py
+++ ReTwitter/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'ReTwitter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 
 # Password validation
@@ -127,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
+
